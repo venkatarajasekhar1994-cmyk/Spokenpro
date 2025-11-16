a1a04aef6ab298404b134cba8fc44a4fe9984108
venkatarajasekhar1994-cmyk <venkatarajasekhar1994@gmail.com>
2025-11-17 01:18:30 +0530

    Update service worker to new cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'speakpro-glass-v1';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'speakpro-v2';
+const ASSETS = [
   './index.html',
-  './1000094592.jpg',
   './manifest.json',
+  './1000094592.jpg',
   'https://unpkg.com/react@18/umd/react.development.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
@@ -11,41 +11,10 @@ const ASSETS_TO_CACHE = [
   'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&family=Ramabhadra&display=swap'
 ];
 
-// Install Event: Cache assets
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Fetch Event: Serve from cache, then network
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response; // Hit cache
-      }
-      return fetch(event.request); // Miss cache, hit network
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
-
-// Activate Event: Clean up old caches
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
